b10dad5d537b4a706d6a86c81e2c908e94a96534
Lineker Valim <81521229+lineker1@users.noreply.github.com>
2023-04-20 16:21:16 -0300

    Criação e autenticação de usario e senha

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -0,0 +1,73 @@
+package br.com.alura.orgs.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import br.com.alura.orgs.database.AppDatabase
+import br.com.alura.orgs.databinding.ActivityListaProdutosActivityBinding
+import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+
+class ListaProdutosActivity : AppCompatActivity() {
+
+    private val adapter = ListaProdutosAdapter(context = this)
+    private val binding by lazy {
+        ActivityListaProdutosActivityBinding.inflate(layoutInflater)
+    }
+    private val produtoDao by lazy {
+        val db = AppDatabase.instancia(this)
+        db.produtoDao()
+    }
+
+    private val usuarioDao by lazy {
+        AppDatabase.instancia(this).usuarioDao()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        configuraRecyclerView()
+        configuraFab()
+        lifecycleScope.launch {
+            launch {
+                produtoDao.buscaTodos().collect { produtos ->
+                    adapter.atualiza(produtos)
+                }
+            }
+            intent.getStringExtra("CHAVE_USUARIO_ID")?.let { usuarioId ->
+                usuarioDao.buscaPorId(usuarioId).collect {
+                }
+            }
+        }
+    }
+
+    private fun configuraFab() {
+        val fab = binding.activityListaProdutosFab
+        fab.setOnClickListener {
+            vaiParaFormularioProduto()
+        }
+    }
+
+    private fun vaiParaFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = binding.activityListaProdutosRecyclerView
+        recyclerView.adapter = adapter
+        adapter.quandoClicaNoItem = {
+            val intent = Intent(
+                this,
+                DetalhesProdutoActivity::class.java
+            ).apply {
+                putExtra(CHAVE_PRODUTO_ID, it.id)
+            }
+            startActivity(intent)
+        }
+    }
+
+}
